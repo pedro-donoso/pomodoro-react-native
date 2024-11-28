8db54543e6c5b6bacae7d90c3fd5dd1ed2f14b21
pedro-donoso <pedro.donoso.r@usach.cl>
2024-11-28 15:09:03 -0300

    agrego tiempo

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,20 @@
 import { Button, StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
 
-export default function Page() {
+const colors = ["#f7dc6f", "#a2d9ce", "#d7bde2"];
+
+export default function App() {
+  const [isWorking, setIsWorking] = useState(false);
+  const [time, setTime] = useState(25 * 60);
+  const [currentTime, setCurrentTime] = useState("POMO" | "SHORT" | "BREAK");
+
+  
+  
  return (
   <View style={styles.container}>
    <View style={styles.main}>
-    <Text style={styles.title}>Hola mundo</Text>
-    <Button title="My Button" />
+    <Text style={styles.text}>Pomodoro</Text>
+    <Text style={styles.subtitle}>{time}</Text>
    </View>
   </View>
  );
@@ -15,15 +24,16 @@ const styles = StyleSheet.create({
  container: {
   flex: 1,
   alignItems: "center",
-  padding: 24,
+  padding: 24
  },
  main: {
   flex: 1,
   justifyContent: "center",
   maxWidth: 960,
   marginHorizontal: "auto",
+  
  },
- title: {
+ text: {
   fontSize: 32,
   fontWeight: "bold",
  },
