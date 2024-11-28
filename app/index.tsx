30a29c6a2e222a0b6f5592aa69e2a4a1795f2de7
pedro-donoso <pedro.donoso.r@usach.cl>
2024-11-28 14:38:30 -0300

    agrego boton

--- app/index.tsx
+++ app/index.tsx
@@ -1,34 +1,34 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function Page() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.main}>
-        <Text style={styles.title}>Hola mundo</Text>
-        <Text style={styles.subtitle}>Bienvenidos</Text>
-      </View>
-    </View>
-  );
+ return (
+  <View style={styles.container}>
+   <View style={styles.main}>
+    <Text style={styles.title}>Hola mundo</Text>
+    <Button title="My Button" />
+   </View>
+  </View>
+ );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    padding: 24,
-  },
-  main: {
-    flex: 1,
-    justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
-  },
-  title: {
-    fontSize: 32,
-    fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 24,
-    color: "#38434D",
-  },
+ container: {
+  flex: 1,
+  alignItems: "center",
+  padding: 24,
+ },
+ main: {
+  flex: 1,
+  justifyContent: "center",
+  maxWidth: 960,
+  marginHorizontal: "auto",
+ },
+ title: {
+  fontSize: 32,
+  fontWeight: "bold",
+ },
+ subtitle: {
+  fontSize: 24,
+  color: "#38434D",
+ }
 });
