0a2e447fde9e83e480b91b159573331e67bbc0e6
pedro-donoso <pedro.donoso.r@usach.cl>
2024-11-28 17:05:26 -0300

    actualizo proyecto

--- app/index.tsx
+++ app/index.tsx
@@ -3,19 +3,24 @@ import { useState } from "react";
 import Header from "../components/Header";
 
 const colors = ["#f7dc6f", "#a2d9ce", "#d7bde2"];
+const options = ["Pomodoro", "Short Break", "Long Break"];
 
 export default function App() {
-  const [isWorking, setIsWorking] = useState(false);
-  const [time, setTime] = useState(25 * 60);
-  const [currentTime, setCurrentTime] = useState("POMO" | "SHORT" | "BREAK");
+ const [isWorking, setIsWorking] = useState(false);
+ const [time, setTime] = useState(25 * 60);
+ const [currentTime, setCurrentTime] = useState(0);
 
-  
-  
+ const formatTime = (seconds: number) => {
+  const minutes = Math.floor(seconds / 60);
+  const secs = seconds % 60;
+  return `${minutes < 10 ? "0" : ""}${minutes}:${secs < 10 ? "0" : ""}${secs}`;
+ };
+
  return (
-  <View style={[styles.container, {backgroundColor: colors[currentTime]}]}>
+  <View style={[styles.container, { backgroundColor: colors[currentTime] }]}>
    <View style={styles.main}>
     <Text style={styles.text}>Pomodoro</Text>
-    <Text style={styles.text}>{time}</Text>
+    <Text style={styles.text}>{formatTime(time)}</Text>{" "}
     <Header
      currentTime={currentTime}
      setCurrentTime={setCurrentTime}
@@ -30,14 +35,13 @@ const styles = StyleSheet.create({
  container: {
   flex: 1,
   alignItems: "center",
-  padding: 24
+  padding: 24,
  },
  main: {
   flex: 1,
   justifyContent: "center",
   maxWidth: 960,
   marginHorizontal: "auto",
-  
  },
  text: {
   fontSize: 32,
@@ -46,5 +50,5 @@ const styles = StyleSheet.create({
  subtitle: {
   fontSize: 24,
   color: "#38434D",
- }
+ },
 });
