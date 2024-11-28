842324fedd4293d2d2e74978fd068ec2dbb9ee96
pedro-donoso <pedro.donoso.r@usach.cl>
2024-11-28 15:34:13 -0300

    muestro opciones

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,15 @@
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 
-export default function Header({time}) {
+const options = ["Pomodoro", "Short Break", "Long Break"];
+
+export default function Header({ time }) {
   return (
     <View>
-      <Text>{time}</Text>
+      {options.map((item, index) => (
+        <TouchableOpacity key={index}>
+          <Text>{item}</Text>
+        </TouchableOpacity>
+      ))}
     </View>
   )
 }
