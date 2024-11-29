6bfe44a5e1d19a9e538d8620894531ecf598b6ee
pedro-donoso <pedro.donoso.r@usach.cl>
2024-11-29 10:34:02 -0400

    formateo time

--- components/Timer.jsx
+++ components/Timer.jsx
@@ -1,9 +1,15 @@
 import { View, Text, StyleSheet } from "react-native"
 
 export default function Timer({time}) {
+  const formattedTime = `${Math.floor(time / 60)
+    .toString()
+    .padStart(2, "0")}:${(time % 60)
+      .toString()
+      .padStart(2, "0")
+    }`;
   return (
     <View style={styles.container}>
-        <Text style={styles.time}>{time}</Text>
+        <Text style={styles.time}>{formattedTime}</Text>
     </View>
   )
 }
@@ -19,6 +25,7 @@ const styles = StyleSheet.create({
   time: {
     fontSize: 80,
     fontWeight: "bold",
-    textAlign: "center"
+    textAlign: "center",
+    color: "#333333"
   }
 })
